202017e87e5ad108aab93f2b4a43037d372b3014
Justin Z <contactjustinzamora@gmail.com>
2021-01-26 18:16:32 -0800

    added notes for the components

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,11 +1,14 @@
 import React, {useState} from 'react'
 import UserForm from './UserForm'
 import User from './User'
+import './Users.css'
 
 const Users =() =>{
     const [users, setUsers] = useState([])
+
     //addUser adds object/user from UserForm component. 
-    //The form includes username and password to add on to the initial state of users.
+    //The form from UserForm includes *username and *password to add on to the initial state of users (array).
+    //It is passed down as a prop for UserForm and receives back the information to append to the User component. 
     const addUser = (user) =>{
         setUsers(users => [...users, {...user}])
     }
@@ -14,6 +17,7 @@ const Users =() =>{
         <UserForm addUser={addUser}/>
         <div>
                 {users.map(u => {
+                    //Users UI for any users that are being added.
                     return(
                     <User username={u.username} password={u.password}/>
                     )
